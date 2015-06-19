b44f4569e61b392cc18fa600b1fdb4785b2223aa
Tim Otten <totten@civicrm.org>
2015-06-18 17:11:35 -0700

    Index page should return list of apps.

--- src/Civi/Cxn/AppBundle/Controller/DefaultController.php
+++ src/Civi/Cxn/AppBundle/Controller/DefaultController.php
@@ -2,12 +2,38 @@
 
 namespace Civi\Cxn\AppBundle\Controller;
 
+use Civi\Cxn\Rpc\AppStore\AppStoreInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller {
 
-  public function indexAction($name) {
-    return $this->render('CiviCxnAppBundle:Default:index.html.twig', array('name' => $name));
+  /**
+   * @var AppStoreInterface
+   */
+  private $appStore;
+
+  public function __construct(ContainerInterface $container, AppStoreInterface $appStore) {
+    $this->setContainer($container);
+    $this->appStore = $appStore;
+  }
+
+  public function indexAction() {
+    $out = '';
+
+    foreach ($this->appStore->getAppIds() as $appId) {
+      $appMeta = $this->appStore->getAppMeta($appId);
+      $out .= sprintf("== %s (%s) ==\n\n%s\n\n", $appMeta['title'], $appMeta['appId'], $appMeta['desc']);
+    }
+
+    return new Response($out, 200, array(
+      'Content-type' => 'text/plain',
+    ));
+
+    //return $this->render('CiviCxnAppBundle:Default:index.html.twig', array(
+    //  'appMetas' => $appMetas,
+    //));
   }
 
 }
