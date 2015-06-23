9b42bed1a6a8f812893bded4975eec1921de2d00
Tim Otten <totten@civicrm.org>
2015-06-22 20:22:52 -0700

    Import `dirsvc:preview` command

---
+++ src/Civi/Cxn/DirBundle/Command/PreviewCommand.php
@@ -0,0 +1,25 @@
+<?php
+namespace Civi\Cxn\DirBundle\Command;
+
+use Civi\Cxn\Rpc\AppMeta;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PreviewCommand extends Command {
+
+  protected function configure() {
+    $this
+      ->setName('dirsvc:preview')
+      ->setDescription('Preview the metadata of a remote application')
+      ->setHelp('Example: cxndir preview http://app.example.com/cxn/metadata.json')
+      ->addArgument('url', InputArgument::REQUIRED, 'The application\'s metadata URL');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $appMeta = json_decode(file_get_contents($input->getArgument('url')), TRUE);
+    AppMeta::validate($appMeta);
+    print_r($appMeta);
+  }
+}
